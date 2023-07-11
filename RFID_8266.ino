69c6de38c8ea4fff6497b2f3e594ffe3153891da
RachataS <64030161@kmitl.ac.th>
2023-07-11 10:52:35 +0700

    change file name

--- RFID_8266.ino
+++ RFID_8266.ino
@@ -1,5 +1,9 @@
 #include <SPI.h>
 #include "MFRC522.h"
+#include <ESP8266WiFi.h>
+
+#define WIFI_SSID "Myangle_2.4G" 
+#define WIFI_PASSWORD "0819879414"
 
 #define RST_PIN  D1
 #define SS_PIN  D2
@@ -9,6 +13,7 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 String rfid_in = "";
 
 void setup() {
+  connectWifi();
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
@@ -33,4 +38,18 @@ String rfid_read() {
   }
   content.toUpperCase();
   return content.substring(1);
+}
+
+void connectWifi() {
+    Serial.begin(9600);
+    Serial.println(WiFi.localIP());
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("connecting");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println();
+    Serial.print("connected: ");
+    Serial.println(WiFi.localIP());
 }
